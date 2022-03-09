75794703f6b0d702eb1e24c66350e1430b96ab3c
Kelechi Uchegbu <knuchegbu@spscommerce.com>
2022-03-09 08:34:26 -0600

    added multiple features  - built out the list component  - built out the map component  - built out the PlaceDetails component

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -6,9 +6,15 @@ import Rating from "@material-ui/lab/Rating";
 
 import useStyles from "./MapStyles";
 
-const Map = ({ setCoordinates, setBounds, coordinates }) => {
+const Map = ({
+  setCoordinates,
+  setBounds,
+  coordinates,
+  places,
+  setChildClicked,
+}) => {
   const classes = useStyles();
-  const isMobile = useMediaQuery("(min-width:600px)"); //set to false if not mobile
+  const isDesktop = useMediaQuery("(min-width:600px)"); //set to false if not mobile
 
   return (
     <div className={classes.mapContainer}>
@@ -16,15 +22,50 @@ const Map = ({ setCoordinates, setBounds, coordinates }) => {
         bootstrapURLKeys={{ key: "AIzaSyBkt95utznWiIfQWrLxruEhixClZdhovDw" }}
         defaultCenter={coordinates}
         center={coordinates}
-        defaultZoom={isMobile ? 15 : 10}
+        defaultZoom={isDesktop ? 15 : 10}
         margin={[0, 0, 0, 0]}
         options={""}
         onChange={(e) => {
           setCoordinates({ lat: e.center.lat, lng: e.center.lng });
           setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
         }}
-        onChildClick={""}
-      ></GoogleMapReact>
+        onChildClick={(child) => {
+          setChildClicked(child);
+        }}
+      >
+        {places?.map((place, i) => (
+          <div
+            className={classes.markerContainer}
+            lat={Number(place.latitude)}
+            lng={Number(place.longitude)}
+            key={i}
+          >
+            {!isDesktop ? (
+              <LocationOnOutlinedIcon color="primary" fontSize="large" />
+            ) : (
+              <Paper elevation={3} className={classes.paper}>
+                <Typography
+                  className={classes.typography}
+                  variant="subtitle2"
+                  gutterBottom
+                >
+                  {place.name}
+                </Typography>
+                <img
+                  className={classes.pointer}
+                  alt={place?.name}
+                  src={
+                    place?.photo
+                      ? place.photo.images.large.url
+                      : "https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg"
+                  }
+                />
+                <Rating size="small" value={Number(place.rating)} readOnly />
+              </Paper>
+            )}
+          </div>
+        ))}
+      </GoogleMapReact>
     </div>
   );
 };
