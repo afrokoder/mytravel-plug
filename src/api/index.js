aa69a42ba7a7189640ad7b7a3dcf7a679dc8eb53
Kelechi Uchegbu <knuchegbu@spscommerce.com>
2022-03-09 13:56:56 -0600

    secure keys with ENV variables and depracate old keys

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,9 @@
 import axios from "axios";
-
+import apiKeys from "./api-keys";
 const URL =
   "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
-
+// const RAPID_API_KEY = process.env.RAPID_API_KEY;
+// console.log("rapid", RAPID_API_KEY);
 export const getPlacesData = async (sw, ne) => {
   try {
     const {
@@ -16,7 +17,7 @@ export const getPlacesData = async (sw, ne) => {
       },
       headers: {
         "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
-        "x-rapidapi-key": "1e2b6572d5mshe31986154d35f66p1197c9jsna213b437cc6c",
+        "x-rapidapi-key": apiKeys.RAPID_API_KEY,
       },
     });
     return data;
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import GoogleMapReact from "google-map-react";
+import apiKeys from "../../api/api-keys";
 import { Paper, Typography, useMediaQuery } from "@material-ui/core";
 import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
 import Rating from "@material-ui/lab/Rating";
@@ -9,12 +10,14 @@ import useStyles from "./MapStyles";
 const Map = ({ setCoordinates, setBounds, coordinates }) => {
   const classes = useStyles();
   const isMobile = useMediaQuery("(min-width:600px)"); //set to false if not mobile
-  const API_KEY = process.env.GOOGLE_MAPS_API_KEY;
+  // const API_KEY = process.env.GOOGLE_MAPS_API_KEY;
+  // console.log(API_KEY);
+  console.log(process.env);
 
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: API_KEY }}
+        bootstrapURLKeys={{ key: apiKeys.GOOGLE_MAPS_API_KEY }}
         defaultCenter={coordinates}
         center={coordinates}
         defaultZoom={isMobile ? 15 : 10}
